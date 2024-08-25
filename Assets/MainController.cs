26b7f4fecb979d357abd8ad9a797c812c4b0af50
Andres Toala <ai_toala@outlook.com>
2024-08-25 17:40:06 -0500

    all games included

--- Assets/MainController.cs
+++ Assets/MainController.cs
@@ -93,6 +93,11 @@ public class MainController : MonoBehaviour
         games = new List<string>
         {
             "SecuenciaGame",
+            "SecuenciaGame",
+            "IntrusoGame",
+            "IntrusoGame",
+            "RecogeManzanas",
+            "RecogeManzanas",
         };
         currentGame = GetNextGame();
         Debug.Log("Starting game: " + currentGame);
